3c7d9047c23816e282feb0609db180e82b26665b
Daniel White <dan.white@salesforce.com>
2016-06-27 01:08:00 -0300

    first commit

---
+++ Assets/Network.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using SocketIO;
+
+public class Network : MonoBehaviour {
+	public GameObject playerPrefab;
+	static SocketIOComponent socket;
+
+	// Use this for initialization
+	void Start () {
+		socket = GetComponent<SocketIOComponent> ();
+		socket.On ("open", OnConnected);
+		socket.On ("spawn", OnSpawned);
+	}
+	
+	void OnConnected(SocketIOEvent e){
+		Debug.Log ("connected");
+		socket.Emit ("move");
+	}
+
+	void OnSpawned (SocketIOEvent e) {
+		Debug.Log ("Spawned");
+		Instantiate (playerPrefab);
+	}
+}
